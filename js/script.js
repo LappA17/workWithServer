c1d83f32fc3d6b33507ad3cc5f8c76473c6c1e62
Ruslan <ruslan.postoyuk@gmail.com>
2022-02-11 22:57:27 +0100

    58.formData + XMLHttpRequest и это все на php и mump

--- js/script.js
+++ js/script.js
@@ -1,93 +1,47 @@
    window.addEventListener("DOMContentLoaded", () => {
 
-    // Теперь работаем с методом POST(отправка данных на сервер/ те данные прийдут в db.json + с помощью php отправит formData)
-
-/* Создадим кнопку submit в форме и ВСЕГДА ЕСЛИ Я ПРЕДПОЛОГАЮ ЧТО С МОИМИ ИНПУТАМИ БУДЕТ КАКОЕ-ТО СЕРВЕРНОЕ ВЗАИМОДЕЙСТВИЕ - ТО ВСЕГДА
-НАЗНАЧАТЬ АТРИБУТ name, по нему идет ориентация, без него сервер не будет работать */
-
+    // ТЕПЕРЬ ОТПРАВЛЯЕМ formData + подключаем php сервер для его отправки
+    // Так же подключаем MUMP
     const form = document.querySelector('form');
 
     function req(e) {
         e.preventDefault();
 
-        // Теперь получаем данные из form 
-        let formData = new FormData(form); // во внутрь помещаем ту форму которую мы хотим получить
-        formData.append("id", Math.random()); // Помни что json всегда просит id, а math.random - его значение
-/* При создание объекта который мы будем постить на наш сервер json сервер обязательно требует что бы у нашего объекта 
-который мы присылаем на сервер был ID !  */
+        let formData = new FormData(form);
         
-        // Но мы не можем просто так взять и превратить формДейта в дсон объект, let json = JSON.stringify(formData) не сработает
-        let obj = {};
-        formData.forEach((value, key) => {
-            // Расскрываем эту коллбек фцию и говорим что я буду заполнять мой новый объект вот таким вот образом
-            obj[key] = value; // Таким образом obj будет заполнен всем тем что было внутри formData
-        });
-        // let json = JSON.stringify(obj); 
-        /* {
-            "name": "Руслан Потоюк",
-            "age": "38099",
-            "id": "0.13823632680258857"
-          } Теперь те данные которые я ввел в инпут вернутьс нам в db.json
-    То-есть сначало нужно получить форму с которой будем работать с html const form = document.querySelector('form');
-    Дальше навесить обработчик события submit и передать объект события. Отменяем станд повед браузера 
-    Дальше при помощи объекта formData мы получаем все данные из определенной формы new FormData(form)
-    Добавляем уникальный айди(если сервер требует это)
-    Дальше что бы наш formData сформировать в виде json мы провели мохинацию с obj, создаем пустой объект и при помощи forEach перекладываем
-в него все значения и потом самый обычный объект трасформируем при помощи джсон стрингифай и потом именно этот json как переменую
-мы отправляем на сервер request.send(json); */
-
-
-        /* ЗАКОММЕНТИРУЕМ ПОТОМУ ЧТО В СЕРЕДИНЕ УРОКА ВАНЯ ПОКАЖЕТ КАК С ПОМОЩЬЮ fetch и axios сделать тоже самое !
+        /* Есть очень важное правило ! Когда я использую XMLHttpRequest плюс ФОРМДЕЙТУ никогда нельзя указывать заголовок, по этому я 
+      request.setRequestHeader("Content-type", "multipart/form-data"); закомментирую потому что сервер сам автоматически его подставит
+      И теперь после того как я подключил MUMP, в консоле мне выдало array(2) {
+  ["name"]=>
+  string(25) "Руслан Потоюк"
+  ["age"]=>
+  string(8) "php.test"
+}  */
         const request = new XMLHttpRequest();
-        request.open("POST", "http://localhost:3000/people"); 
+        request.open("POST", "./api.php");  // тут уже php, а не локальный сервер с терминала
 
-        request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+        // request.setRequestHeader("Content-type", "multipart/form-data"); // из документации, таким образом сервер поймет что я присылаю
 
-        request.send(json); // говорим то что хоти отправить
+        request.send(formData); // тут формдейту отправляем
 
         request.addEventListener("load", function() {
             if (request.status == 200) {
-                let data = JSON.parse(request.response); // приобразоваем json данные в javascript объект
-                console.log(data);
-                // createCards(data); // и помещаем внутрь данные от сервера
-
+                console.log(request.response); // уже не нужно парсить джсон объект, а просто выводим в консоль ответ сервера
             } else {
                 console.error("Что-то пошло не так");
             }
-        });  */
-
-        /*в getResource после url, нужно передать объект который нам нужно превратить в json, 
-те эту строчку которую мы создали в начали МЫ ИГНОРИУРЕМ let json = JSON.stringify(obj); Мы эту операцию делаем в настройках в fetch*/ 
-         /*getResource("http://localhost:3000/people",  obj)
-            .then(data => createCards(data)) 
-            .catch(err => console.error(err)); */
-/* И все работает {
-      "name": "fetch test",
-      "age": "test fetch",
-      "id": "0.44254062065929234"
-    } */
-
-        axios.post("http://localhost:3000/people",  obj); /* {
-            "name": "Русла axios",
-            "age": "38099",
-            "id": "0.9325442333885843"
-          } 
-        ИЗ ПРОШЛОГО КОММИТА С ФЕТЧЕМ Я ПРОСТО ЗАКОММЕНТИРОВАЛ ТО ЧТО ВЫШЕ И ПРОПИСАЛ ВОТ ЭТУ КОММАНДУ И ПОЛУЧИЛОСЬ СДЕЛАТЬ ПОСТ ЗАПРОС */
+        });  
     }
 
-    // Нужно сказать что событие будет происходить на форме и событие сабмит
-    form.addEventListener("submit", (e) => req(e), {"once": true}); /* Создадим стрелочную фцию которая
-в свою очередь будет вызывать нашу фцию. Из-за того что мы отправляем форму нам нужно отменить стандартное поведение браузера с перезагрузкой */
+    form.addEventListener("submit", (e) => req(e), {"once": true}); 
 
-
-    // Post запрос через fetch
     async function getResource(url, data) { 
         const res = await fetch(`${url}`, {
             method: "POST",
             headers: {
-                "Content-type": "application/json" // Так как мы отправляем json то заголовок такой, и двоеточие потому что это объект
+                "Content-type": "application/json" 
             },
-            body: JSON.stringify(data) //body - это что мы будем отправлять. data - данные которые нужно отправить, в джсон объекте
+            body: JSON.stringify(data)
         }); 
 
         if (!res.ok) { 
